cae4f1133572da1d16d9689b6e8596a3b8581ceb
alxala <78293441+alxala@users.noreply.github.com>
2022-10-30 16:52:06 +0700

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,3 +1,97 @@
+let dbAds=[
+  {
+    "target-selector":[
+      ".adsot",
+      "#adsot",
+      ".siteorigin-widget-tinymce textwidget"
+    ],
+    "position":"out-top", //out-top, out-bottom, in-top, in-bottom
+    "data" :`
+    <script type="text/javascript">
+	    atOptions = {
+		    'key' : '35ff9bfde9c6692e37b4dc39e859a703',
+		    'format' : 'iframe',
+		    'height' : 250,
+		    'width' : 300,
+        'align' : 'center'
+		    'params' : {}
+	    };
+	    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://wimpmustsyllabus.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></scr' + 'ipt>');
+    </script>
+    `,
+    "style":`
+      width: 90%;
+      margin: auto;
+      margin-bottom: 10px;
+      margin-top: 10px;
+    `
+  }
+];
+
+dbAds.forEach(function(a){
+  let createElDom=document.createElement("div");
+  createElDom.setAttribute("style",a["style"]);
+  createElDom.innerHTML=a["data"];
+  let dataScript=[];
+  createElDom.querySelectorAll("script").forEach(function(b){
+    let createElCostom=document.createElement("script");
+    createElCostom.innerHTML=b.innerHTML;
+    dataScript.push(createElCostom);
+    b.remove();
+  });
+  a["target-selector"].forEach(function(b){
+    let targetEl=document.querySelector(b);
+    if(targetEl){
+      if(a["position"]=="out-bottom"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+      }else if(a["position"]=="out-top"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+        createElDom.after(targetEl);
+      }else if(a["position"]=="in-top"){
+
+      }else if(a["position"]=="in-top"){
+        
+      };
+      dataScript.forEach(function(b){
+        createElDom.append(b); 
+      });
+    }else{
+      console.log("target "+a["target-selector"]+" tidak ditemukan"); 
+    };
+  });
+});
+
+
+let elImg=document.querySelectorAll("img");
+elImg.forEach(function(a){
+  a.setAttribute("style","max-width:100%");
+  let classImg=a.getAttribute("class");
+  let getSrcSet=a.getAttribute("srcset");
+  if(classImg==null==false){
+    a.classList.remove("lazyload");
+  };
+  if(getSrcSet==null==false){
+    getSrcSet=getSrcSet.split(",");
+    if(getSrcSet.length>1){
+      a.setAttribute("src",getSrcSet);
+    };
+  };
+});
+
+let dataLazy=document.querySelectorAll(".lazy-image.lazy-image-udf");
+dataLazy.forEach(function(a){
+  let dataHref=a.getAttribute("data-src");
+  if(dataHref){
+    let targetLazy=a.querySelector(".loadingPlaceholder");
+    let targetDiv=a.querySelector(".lazy-image__loadingPlaceholder")
+    if(targetLazy){
+      targetLazy.setAttribute("src",dataHref);
+      targetDiv.setAttribute("class","show")
+    };
+  };
+});
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"var _Hasync= _Hasync|| [];\n_Hasync.push(['Histats.start', '1,4571407,4,0,0,0,00010000']);\n_Hasync.push(['Histats.fasi', '1']);\n_Hasync.push(['Histats.track_hits', '']);\n(function() {\nvar hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;\nhs.src = ('//s10.histats.com/js15_as.js');\n(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);\n})();"},{"attr":[],"tag":"noscript","inner":"<a href=\"/\" target=\"_blank\"><img src=\"//sstatic1.histats.com/0.gif?4571407&amp;101\" alt=\"web counter\" border=\"0\"></a>"}],{"target":"footer"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"var _Hasync= _Hasync|| [];\n_Hasync.push(['Histats.start', '1,4710974,4,0,0,0,00010000']);\n_Hasync.push(['Histats.fasi', '1']);\n_Hasync.push(['Histats.track_hits', '']);\n(function() {\nvar hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;\nhs.src = ('//s10.histats.com/js15_as.js');\n(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);\n})();"},{"attr":[],"tag":"noscript","inner":"<a href=\"/\" target=\"_blank\"><img src=\"//sstatic1.histats.com/0.gif?4710974&amp;101\" alt=\"\" border=\"0\"></a>"}],{"target":"footer"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 (function(){injectScriptnya([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\natOptions = {\n'key' : '35ff9bfde9c6692e37b4dc39e859a703',\n'format' : 'iframe',\n'height' : 250,\n'width' : 300,\n'params' : {}\n};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"https://www.highperformancedisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"}],"tag":"script","inner":""}],{"target":[".adsot","#adsot",".adsot"]});function injectScriptnya(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
