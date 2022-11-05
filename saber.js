aba8e18af66cb8ef59285ace20a7a233529de911
alxala <78293441+alxala@users.noreply.github.com>
2022-11-06 06:14:53 +0700

    Update and rename balmon.js to saber.js

--- saber.js
+++ saber.js
@@ -1,4 +1,4 @@
-ad="<script type=&#34;text/javascript&#34;>
+var adc ="<script type=&#34;text/javascript&#34;>
 	atOptions = {
 		&#39;key&#39; : &#39;35ff9bfde9c6692e37b4dc39e859a703&#39;,
 		&#39;format&#39; : &#39;iframe&#39;,
@@ -9,9 +9,8 @@ ad="<script type=&#34;text/javascript&#34;>
 	document.write(&#39;<scr&#39; + &#39;ipt type=&#34;text/javascript&#34; src=&#34;http&#39; + (location.protocol === &#39;https:&#39; ? &#39;s&#39; : &#39;&#39;) + &#39;://www.profitabledisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js&#34;></scr&#39; + &#39;ipt>&#39;);
 </script>"
 
-document.write(info);
 
-info="<!-- Histats.com  START  (aync)-->
+var info = "<!-- Histats.com  START  (aync)-->
 <script type=&#34;text/javascript&#34;>var _Hasync= _Hasync|| [];
 _Hasync.push([&#39;Histats.start&#39;, &#39;1,4710974,4,0,0,0,00010000&#39;]);
 _Hasync.push([&#39;Histats.fasi&#39;, &#39;1&#39;]);
@@ -24,4 +23,3 @@ hs.src = (&#39;//s10.histats.com/js15_as.js&#39;);
 <noscript><a href=&#34;/&#34; target=&#34;_blank&#34;><img  src=&#34;//sstatic1.histats.com/0.gif?4710974&101&#34; alt=&#34;&#34; border=&#34;0&#34;></a></noscript>
 <!-- Histats.com  END  -->"
 
-document.write(info);
