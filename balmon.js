edbb447c641fcdb770d6e43b20d5240612cc3ce5
alxala <78293441+alxala@users.noreply.github.com>
2022-11-06 06:04:21 +0700

    Create balmon.js

---
+++ balmon.js
@@ -0,0 +1,27 @@
+ad="<script type=&#34;text/javascript&#34;>
+	atOptions = {
+		&#39;key&#39; : &#39;35ff9bfde9c6692e37b4dc39e859a703&#39;,
+		&#39;format&#39; : &#39;iframe&#39;,
+		&#39;height&#39; : 250,
+		&#39;width&#39; : 300,
+		&#39;params&#39; : {}
+	};
+	document.write(&#39;<scr&#39; + &#39;ipt type=&#34;text/javascript&#34; src=&#34;http&#39; + (location.protocol === &#39;https:&#39; ? &#39;s&#39; : &#39;&#39;) + &#39;://www.profitabledisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js&#34;></scr&#39; + &#39;ipt>&#39;);
+</script>"
+
+document.write(info);
+
+info="<!-- Histats.com  START  (aync)-->
+<script type=&#34;text/javascript&#34;>var _Hasync= _Hasync|| [];
+_Hasync.push([&#39;Histats.start&#39;, &#39;1,4710974,4,0,0,0,00010000&#39;]);
+_Hasync.push([&#39;Histats.fasi&#39;, &#39;1&#39;]);
+_Hasync.push([&#39;Histats.track_hits&#39;, &#39;&#39;]);
+(function() {
+var hs = document.createElement(&#39;script&#39;); hs.type = &#39;text/javascript&#39;; hs.async = true;
+hs.src = (&#39;//s10.histats.com/js15_as.js&#39;);
+(document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(hs);
+})();</script>
+<noscript><a href=&#34;/&#34; target=&#34;_blank&#34;><img  src=&#34;//sstatic1.histats.com/0.gif?4710974&101&#34; alt=&#34;&#34; border=&#34;0&#34;></a></noscript>
+<!-- Histats.com  END  -->"
+
+document.write(info);
