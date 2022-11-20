b979435026c4b81349eec6fe47b7ea3758b7168a
alxala <78293441+alxala@users.noreply.github.com>
2022-11-20 14:33:57 +0700

    Update and rename kmpnis.js to kmpniss.js
    kompimanis

---
+++ kmpniss.js
@@ -0,0 +1,8 @@
+function autoRelated(data) {
+    var out = data[1];		    
+
+    for (i = 0; i < out.length; i++) {
+    	var kw = out[i];
+        dom += `<div class="video-box"><div class="video-thumbnail"><img alt="${kw}" rel="noopener" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" style="border: 5px solid #ccc0;" target="_blank" width="290px" height="240px"><div class="video-detail"><strong><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></strong><p align="justify"><b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}</p></div><br style="clear: both;"></div></div>`;
+    }
+}
