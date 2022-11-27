4df4b0496e4e443a9491587d08ba58bec53f5cc7
alxala <78293441+alxala@users.noreply.github.com>
2022-11-27 13:49:26 +0700

    Create jd.js

---
+++ jd.js
@@ -0,0 +1,8 @@
+function autoRelated(data) {
+    var out = data[1];		    
+
+    for (i = 0; i < out.length; i++) {
+    	var kw = out[i];
+        dom += `<div class="video-box"><div class="video-thumbnail"><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}.jpg"  target="_blank" width="100%" height="100%"><div class="video-detail"><strong><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></strong><p align="justify"><b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}</p></div><br style="clear: both;"></div></div>`;
+    }
+}
